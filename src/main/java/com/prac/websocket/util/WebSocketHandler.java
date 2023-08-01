69dc10d3133d81fe328aef91ae2206ee8e14f685
hyun1024 <haema3043@gmail.com>
2023-08-01 17:12:05 +0900

    feat: 기본 webSocket 기능 구현

---
+++ src/main/java/com/prac/websocket/util/WebSocketHandler.java
@@ -0,0 +1,47 @@
+package com.prac.websocket.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Slf4j
+public class WebSocketHandler extends TextWebSocketHandler {
+
+
+    private static Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload : " + payload);
+
+        for(WebSocketSession sess: sessions.values()) {
+            sess.sendMessage(message);
+        }
+    }
+
+    /* Client가 접속 시 호출되는 메서드 */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+        sessions.put(session.getId(),session);
+
+        log.info(session + " 클라이언트 접속");
+    }
+
+    /* Client가 접속 해제 시 호출되는 메서드드 */
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+
+        log.info(session + " 클라이언트 접속 해제");
+        sessions.remove(session.getId());
+    }
+}
